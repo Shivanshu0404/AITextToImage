eb3bc2b3406d12b3086983e417acd93761dc2a9c
Shivanshu0404 <159226260+Shivanshu0404@users.noreply.github.com>
2025-05-15 12:13:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const key="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"
+const key="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"
 let inputtext=document.querySelector("#input")
 let image=document.querySelector("#img")
 let Genbutton=document.querySelector("#btn")
@@ -6,13 +6,13 @@ const svg=document.getElementById("svg")
 const load=document.getElementById("load")
 const reset=document.getElementById("reset")
 const downloadbtn=document.getElementById("download")
+
 async function query(data) {
 	const response = await fetch(
 		"https://router.huggingface.co/fal-ai/fal-ai/fast-sdxl",
 		{
 			headers: {
-				Authorization: `Bearer ${key}` 
-				
+				Authorization: `Bearer ${key}`,
 			},
 			method: "POST",
 			body: JSON.stringify({"inputs": inputtext.value}),
@@ -23,6 +23,24 @@ async function query(data) {
 	load.style.display="none" 
 	return result;
 }
+
+// async function query(data) {
+// 	const response = await fetch(
+// 		"https://router.huggingface.co/fal-ai/fal-ai/fast-sdxl",
+// 		{
+// 			headers: {
+// 				Authorization: `Bearer ${key}` 
+				
+// 			},
+// 			method: "POST",
+// 			body: JSON.stringify({"inputs": inputtext.value}),
+// 		}
+// 	);
+// 	const result = await response.blob();
+// 	image.style.display='block'
+// 	load.style.display="none" 
+// 	return result;
+// }
 async function generate() {
 load.style.display="block"    
 query().then((response) => {
